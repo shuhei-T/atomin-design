5b4907340d349d216db2d6897b1363eb026f253b
shuhei-T <81573592+shuhei-T@users.noreply.github.com>
2022-06-14 15:10:24 +0900

    Recoilでのstate管理実践

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
-import { UserContext } from "../../../providers/UserProvider";
+import { useRecoilValue } from "recoil";
+
+// import { UserContext } from "../../../providers/UserProvider";
+import { userState } from "../../../store/userState";
 
 export const UserIconWithName = (props) => {
   console.log("UserIconWithName");
   const { image, name } = props;
-  const { userInfo } = useContext(UserContext);
+  // const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
 
   return (
